654ffd3629bde67d9caa9b85ec3bdb0fbdeb09cb
adav <Andrew#7>
2016-09-14 21:55:46 +0100

    Add a transformer test

---
+++ src/test/scala/com/knoldus/typeform/TypeformUtilsTest.scala
@@ -0,0 +1,65 @@
+package com.knoldus.typeform
+
+import java.sql.Date
+
+import org.scalatest.{FunSuite, Matchers}
+
+/**
+  * Created by adav on 14/09/2016.
+  */
+class TypeformUtilsTest extends FunSuite with Matchers {
+
+  test("extracting values from webhook json") {
+
+    val json =
+      """
+        |{
+        |  "uid": "AbCdEfGhIj",
+        |  "token": "440d47cd38ed9234a45cd160649f2203",
+        |  "answers": [
+        |	  {
+        |    	"field_id": 35647923,
+        |		"tags": ["dates"],
+        |		"type": "choices",
+        |		"value":
+        |		  {"labels": ["15 Sep 2016", "29 Sep 2016"],
+        |				  "other": "None"
+        |				 }
+        |	  },
+        |	  {
+        |"field_id": 35647925,
+        |"tags": ["mobile"],
+        |"type": "text",
+        |"value": "0303023023"
+        |	  },
+        |	  {
+        |"field_id": 35647924,
+        |"tags": ["email"],
+        |"type": "text",
+        |"value": "asdasd@asdasdad.com"
+        |	  },
+        |	  {
+        |"field_id": 35647920,
+        |"tags": ["firstname"],
+        |"type": "text",
+        |"value": "Andrew"
+        |	  },
+        |	  {
+        |"field_id": 35647921,
+        |"tags": ["lastname"],
+        |"type": "text",
+        |"value": "David"
+        |	  }
+        |  ]
+        |}
+      """.stripMargin
+
+
+
+    val expected = TypeformResult("Andrew","David","asdasd@asdasdad.com","0303023023",List(Date.valueOf("2016-09-15"), Date.valueOf("2016-09-29")))
+
+    TypeformUtils.processWebhook(json) should be (expected)
+
+  }
+
+}
