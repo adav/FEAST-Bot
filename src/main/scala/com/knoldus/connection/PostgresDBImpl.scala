65f7bf5061e10f30360a72627c4545149eacbefd
adav <Andrew#7>
2016-09-16 00:55:10 +0100

    fix db for heroku

--- src/main/scala/com/knoldus/connection/PostgresDBImpl.scala
+++ src/main/scala/com/knoldus/connection/PostgresDBImpl.scala
@@ -1,24 +1,28 @@
 package com.knoldus.connection
 
 import org.slf4j.LoggerFactory
-import slick.driver.PostgresDriver
 
 trait PostgresDBImpl extends DBComponent {
 
   val driver = slick.driver.PostgresDriver
-  val db: PostgresDriver.backend.Database = PostgresDB.connectionPool
+  val db = PostgresDB.connectionPool
 
 }
 
 private[connection] object PostgresDB {
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  val dbURL = sys.env.getOrElse("DATABASE_URL", "~~FAILED TO READ ENV: DATABASE_URL~~")
+  val dbURL = "jdbc:" + sys.env.getOrElse("DATABASE_URL", "postgres://localhost:5432/?user=adav&password=password").replace("postgres://", "postgresql://")
 
   import slick.driver.PostgresDriver.api._
 
   logger.info(s"Setting up db at DATABASE_URL=$dbURL .......")
 
-  val connectionPool = Database.forURL(url = dbURL)
+  val connectionPool = Database.forURL(url = dbURL, driver = "org.postgresql.Driver" )
+
+  connectionPool.source.createConnection().close()
 
 }
+
+// local dev run:
+// docker run --name feast-postgres -p 5432:5432 -e POSTGRES_PASSWORD=password -e POSTGRES_USER=adav -d postgres
