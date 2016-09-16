66bea33c017ba66c31d47c0499478d341d43c46f
adav <Andrew#7>
2016-09-16 01:09:42 +0100

    fix db for heroku, attempt 2

--- src/main/scala/com/knoldus/connection/PostgresDBImpl.scala
+++ src/main/scala/com/knoldus/connection/PostgresDBImpl.scala
@@ -1,5 +1,7 @@
 package com.knoldus.connection
 
+import java.net.URI
+
 import org.slf4j.LoggerFactory
 
 trait PostgresDBImpl extends DBComponent {
@@ -10,9 +12,10 @@ trait PostgresDBImpl extends DBComponent {
 }
 
 private[connection] object PostgresDB {
+
   val logger = LoggerFactory.getLogger(this.getClass)
 
-  val dbURL = "jdbc:" + sys.env.getOrElse("DATABASE_URL", "postgres://localhost:5432/?user=adav&password=password").replace("postgres://", "postgresql://")
+  val dbURL = getHerokuDbUrl
 
   import slick.driver.PostgresDriver.api._
 
@@ -21,6 +24,18 @@ private[connection] object PostgresDB {
   val connectionPool = Database.forURL(url = dbURL, driver = "org.postgresql.Driver" )
 
   connectionPool.source.createConnection().close()
+
+  private def getHerokuDbUrl: String = sys.env.get("DATABASE_URL") match {
+    case Some(herokuUrl) =>
+      val uri = new URI(herokuUrl)
+
+      val username = uri.getUserInfo.split(":").head
+      val password = uri.getUserInfo.split(":").tail
+
+      s"jdbc:postgresql://${uri.getHost}:${uri.getPort}${uri.getPath}?user=$username&password=$password"
+
+    case None => "jdbc:postgresql://localhost:5432/?user=adav&password=password"
+  }
 
 }
 
