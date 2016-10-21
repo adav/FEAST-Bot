e42cb23eca86e1b9cc63ca5f46af21f3b6aba9d3
adav <Andrew#7>
2016-10-21 23:44:05 +0100

    typeform markdown and linespace fix

--- src/main/scala/com/feastwithus/typeform/TypeformUtils.scala
+++ src/main/scala/com/feastwithus/typeform/TypeformUtils.scala
@@ -24,10 +24,7 @@ object TypeformUtils {
       |  "fields": [
       |    {
       |      "type": "statement",
-      |      "question": "Awesome, you're interested in volunteering with FEAST!
-      |      Thank you!
-      |
-      |      Now, let's get started..."
+      |      "question": "Awesome, you're interested in volunteering with FEAST!\\n\\nThank you!\\n\\n__Now, let's get started...__"
       |    },
       |    {
       |        "type": "short_text",
@@ -44,20 +41,8 @@ object TypeformUtils {
       |    },
       |    {
       |      "type": "statement",
-      |      "question": "Lovely to meet you, {{name}}. Here's some background...",
-      |      "description": "Every Thursday. we:
-      |
-      |* Save surplus food donated from the local Sainsburys and Aldi that would otherwise be thrown in the bin! WOW!
-      |* Take it all to [Conway House](http://irishcentrehousing.org/our-services/supported-services/conway-house), a local hostel in Kilburn...
-      |* ... and turn it all into a delicious and nutritious, communal, vegetarian FEAST! that we all gobble up together!
-      |
-      |__Fellow FEAST!ers, no cooking skills required, ad-hoc commitment; all you need is love!__
-      |
-      |We have only been running for a year and a half, but have already won a prestigious Mitzvah Day award – for most-sustainable project - and are attracting volunteers from all walks of life to join in the fun!
-      |
-      |We aim to start at 5.30pm and wrap-up by 8.30pm, but you are welcome to join for as much/little as you can commit!
-      |
-      |Want to volunteer sometime? It would be wonderful to see you at FEAST!"
+      |      "question": "Lovely to meet you, {{name}}. \\nEvery Thursday, we save surplus food donated from the local Sainsburys and Aldi that would otherwise be thrown in the bin! WOW!\\nThen we take it all to [Conway House](http://irishcentrehousing.org/our-services/supported-services/conway-house), a local hostel in Kilburn...\\n... and turn it all into a delicious and nutritious, communal, vegetarian FEAST! that we all gobble up together!",
+      |      "description": "__Fellow FEAST!ers, no cooking skills required, ad-hoc commitment; all you need is love!__\\n\\nWe have only been running for a year and a half, but have already won a prestigious Mitzvah Day award – for most-sustainable project - and are attracting volunteers from all walks of life to join in the fun!\\n\\nWe aim to start at 5.30pm and wrap-up by 8.30pm, but you are welcome to join for as much/little as you can commit!"
       |    },
       |    {
       |      "type": "multiple_choice",
@@ -70,8 +55,7 @@ object TypeformUtils {
       |    {
       |        "type": "yes_no",
       |        "question": "Are you happy to facilitate for all the Thursdays you have ticked on this form?",
-      |        "description": "This one’s only for those of you who have already been designated as *facilitators*.
-      |        (Please just ignore this question if you haven’t specifically been asked.)",
+      |        "description": "This one’s only for those of you who have already been designated as *facilitators*.\\n(Please just ignore this question if you haven’t specifically been asked.)",
       |        "tags": ["facilitator"]
       |    },
       |    {
